4d347fe96200674f79316fdf68e1dced4c49c9ea
bagas-a <65000032+bagas-a@users.noreply.github.com>
2022-07-30 21:27:47 +0700

    Create STM32F411CEU6_Serial_Monitor.ino

---
+++ STM32/STM32F411CEU6 Serial Monitor/STM32F411CEU6_Serial_Monitor.ino
@@ -0,0 +1,14 @@
+/* 
+ * Program Serial Monitor for STM32
+ * Programming STM32F411CEU6 Serial Monitor using Arduino IDE
+ *
+ */
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  Serial.println("Hello World from DIYSTRON");
+  delay(1000);
+}
