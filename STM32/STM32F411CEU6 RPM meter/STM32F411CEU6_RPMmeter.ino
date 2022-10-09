93e1d6098390d09df6dc79474fa7c916f0a42fe0
bagasarya93 <65000032+bagasarya93@users.noreply.github.com>
2022-10-09 09:07:39 +0700

    Update STM32F411CEU6_RPMmeter.ino

--- STM32/STM32F411CEU6 RPM meter/STM32F411CEU6_RPMmeter.ino
+++ STM32/STM32F411CEU6 RPM meter/STM32F411CEU6_RPMmeter.ino
@@ -5,20 +5,21 @@ float rpm = 0;
 
 int pid;
 unsigned long millisBefore;
-volatile int holes;
+volatile int holes; 
+float pickup = 1.0; //number of pick-up sensors
 
 void setup()
 {
   Serial.begin(115200);
   pinMode(PA0, INPUT);
-  attachInterrupt(digitalPinToInterrupt(PA0), count, FALLING);
+  attachInterrupt(digitalPinToInterrupt(PA0), count, FALLING);//In Love <3
   delay(1000);
 }
 
 void loop()
 {
-  if (millis() - millisBefore > 60000) {
-    rpm = holes / 12.0;
+  if (millis() - millisBefore > 1000) {
+    rpm = (holes / pickup)*60;
     holes = 0;
     millisBefore = millis();
   }
